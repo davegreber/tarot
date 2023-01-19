0d126d42571394b4561c7d12e1501290e89cf11b
Dave Greber <davegreber@gmail.com>
2023-01-19 08:44:49 -0500

    random image works now

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tarot
 
 a tarot card reading site featuring cards which uses objects, technologies and concepts created in the last few years.
+
+should make the dataset a json file when full deck
--- script.js
+++ script.js
@@ -1,46 +1,16 @@
-function display_random_image() 
-{
-     var theImages = [{
-        src: "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
-        width: "240",
-        height: "160"
-    }, {
-        src: "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
-        width: "320",
-        height: "195"
-    }, {
-        src: "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg",
-        width: "500",
-        height: "343"
-    }];
-    
-    var preBuffer = [];
-    for (var i = 0, j = theImages.length; i < j; i++) {
-        preBuffer[i] = new Image();
-        preBuffer[i].src = theImages[i].src;
-        preBuffer[i].width = theImages[i].width;
-        preBuffer[i].height = theImages[i].height;
-    }
-   
-// create random image number
-  function getRandomInt(min,max) 
-    {
-      //  return Math.floor(Math.random() * (max - min + 1)) + min;
-    
-imn = Math.floor(Math.random() * (max - min + 1)) + min;
-    return preBuffer[imn];
-    }  
+let images = [
+    "https://nftstorage.link/ipfs/QmYq8ssfkoWsCTyNU5minSZ5q4GyUQHeEM1x3k1T3SssjZ",
+    "https://nftstorage.link/ipfs/QmTzkq4T5Ew7K6vnP3Z8GW72cZUpatV3ejm4qFa9RD3kJe",
+    "https://nftstorage.link/ipfs/Qmb4U2uqbz8NmeZUopeiXKs2tysksVdwy24a4RiUALyFZw",
+    "https://nftstorage.link/ipfs/QmdEHYPaUzS9dJSg8MpcUvxHsJdosvFap8MzpX2Y7Y8Veu",
+    "https://nftstorage.link/ipfs/QmYGmm3BWmmUkLcBAC1oeoZgCHbuPvfXqzBx17GhMkzU3Y"
+        ];
 
-// 0 is first image,   preBuffer.length - 1) is  last image
-  
-var newImage = getRandomInt(0, preBuffer.length - 1);
- 
-// remove the previous images
-var images = document.getElementsByTagName('img');
-var l = images.length;
-for (var p = 0; p < l; p++) {
-    images[0].parentNode.removeChild(images[0]);
-}
-// display the image  
-document.body.appendChild(newImage);
-}
+  let generateButton = document.getElementById("generate-button");
+  let randomImage = document.getElementById("random-image");
+
+  generateButton.addEventListener("click", function() {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    randomImage.src = images[randomIndex];
+    randomImage.style.display = "block";
+  });
