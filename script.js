18c199731bab4065e2bca1ba4ff552124fadd85b
Dave Greber <davegreber@gmail.com>
2023-01-23 15:05:48 -0500

    cleaned up script

--- script.js
+++ script.js
@@ -6,125 +6,18 @@ let images = [
     {url: "https://nftstorage.link/ipfs/QmYGmm3BWmmUkLcBAC1oeoZgCHbuPvfXqzBx17GhMkzU3Y", text: "5"}
         ];
 
-        var generateButton = document.getElementById("generate-button");
-        var randomImage = document.getElementById("random-image");
-        var imageText = document.getElementById("image-text");
+        let generateButton = document.getElementById("generate-button");
+        let randomImage = document.getElementById("random-image");
+        let imageText = document.getElementById("image-text");
   
         generateButton.addEventListener("click", function() {
-            var randomIndex = Math.floor(Math.random() * images.length);
+            let randomIndex = Math.floor(Math.random() * images.length);
             randomImage.src = images[randomIndex].url;
             //Remove the previous text node if any
             if(imageText.firstChild) imageText.removeChild(imageText.firstChild);
-            var textNode = document.createTextNode(images[randomIndex].text);
+            let textNode = document.createTextNode(images[randomIndex].text);
             imageText.appendChild(textNode);
             randomImage.style.display = "block";
     
   });
 
-/*
-  <!DOCTYPE html>
-<html>
-  <head>
-    <title>Random Image Generator</title>
-    <style>
-        /*Added styles to center the container
-        .container{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        /*added style to center the image and text
-        #random-image, p{
-            margin: 0 auto;
-            text-align: center;
-        }
-    </style>
-  </head>
-  <body>
-    <button id="generate-button">Generate Random Image</button>
-    <br><br>
-    <div class="container">
-        <img id="random-image" style="display: none;">
-        <p id="image-text"></p>
-    </div>
-    <script>
-        var images = [
-            {url: "https://picsum.photos/200", text: "image1"},
-            {url: "https://picsum.photos/200", text: "image2"},
-            {url: "https://picsum.photos/200", text: "image3"},
-            {url: "https://picsum.photos/200", text: "image4"},
-            {url: "https://picsum.photos/200", text: "image5"}
-        ];
-        var generateButton = document.getElementById("generate-button");
-        var randomImage = document.getElementById("random-image");
-        var imageText = document.getElementById("image-text");
-
-        generateButton.addEventListener("click", function() {
-            var randomIndex = Math.floor(Math.random() * images.length);
-            randomImage.src = images[randomIndex].url;
-            var textNode = document.createTextNode(images[randomIndex].text);
-            imageText.appendChild(textNode);
-            randomImage.style.display = "block";
-        });
-    </script>
-  </body>
-</html>
-//</body>
-//
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Random Image Generator</title>
-    <style>
-        /*Added styles to center the container
-        .container{
-          display: flex;
-          justify-content: center;
-          align-items: center;
-      }
-      added style to center the image and text
-      #random-image, p{
-          margin: 0 auto;
-          text-align: center;
-      }
-  </style>
-</head>
-<body>
-  <button id="generate-button">Generate Random Image</button>
-  <br><br>
-  <div class="container">
-      <img id="random-image" style="display: none;">
-      <p id="image-text"></p>
-  </div>
-  <script>
-      var images = [
-          {url: "https://picsum.photos/200", text: "image1"},
-          {url: "https://picsum.photos/200", text: "image2"},
-          {url: "https://picsum.photos/200", text: "image3"},
-          {url: "https://picsum.photos/200", text: "image4"},
-          {url: "https://picsum.photos/200", text: "image5"}
-      ];
-      var generateButton = document.getElementById("generate-button");
-      var randomImage = document.getElementById("random-image");
-      var imageText = document.getElementById("image-text");
-
-      generateButton.addEventListener("click", function() {
-          var randomIndex = Math.floor(Math.random() * images.length);
-          randomImage.src = images[randomIndex].url;
-          //Remove the previous text node if any
-          if(imageText.firstChild) imageText.removeChild(imageText.firstChild);
-          var textNode = document.createTextNode(images[randomIndex].text);
-          imageText.appendChild(textNode);
-          randomImage.style.display = "block";
-      });
-  </script>
-</body>
-</html>
-
-
-
-
-
-
-*/
